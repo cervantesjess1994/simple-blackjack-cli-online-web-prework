b59000d3b4ce4dbb7ebcc093e7542c2a3096db12
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-06-20 08:35:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,48 @@
-def welcome
-  # code #welcome here
+def Welcome
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
-end
+  total = 0
+  total += rand(1..11)
+  end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(sum)
+     puts "Your cards add up to #{sum}."
 end
 
 def prompt_user
-  # code #prompt_user here
-end
+  puts "Type 'h' to hit or 's' to stay"
+  end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
+  return input
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  sum = deal_card
+  sum = sum + deal_card
+  display_card_total(sum)
+  return sum
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    total += deal_card
+  elsif
+    puts invalid_command
+  return total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -41,4 +52,3 @@ end
 def runner
   # code runner here
 end
-    
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require_relative "../lib/blackjack.rb"
-
 RSpec.configure do |config|
 # config here
 end
